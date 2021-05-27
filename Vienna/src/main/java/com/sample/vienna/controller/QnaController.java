33f2afbab72ce35a6bdac77c2260cbe2a747e7a7
MSI\wkdwo <wkdwotjd90@naver.com>
2021-05-27 19:25:15 +0900

    no message

--- Vienna/src/main/java/com/sample/vienna/controller/QnaController.java
+++ Vienna/src/main/java/com/sample/vienna/controller/QnaController.java
@@ -67,17 +67,17 @@ public class QnaController {
 		return "board/qnaBoardRegister";
 	}
 
-	@RequestMapping(value = "/qnaDetail", method = { RequestMethod.GET })
+	@RequestMapping(value = "/qnaDetail", method = { RequestMethod.POST })
 	@ResponseBody
-	public Object boardDetail(HttpServletRequest request, Model model, String code) {
+	public Object qnaDetail(HttpServletRequest request, Model model, String code) {
 		HashMap<String, Object> map = new HashMap<String, Object>();
 		ComBoardVO comBoardVal = new ComBoardVO();
-		comBoardVal.setCode((int) Integer.parseInt(request.getParameter(code)));
+		comBoardVal.setCode((int) Integer.parseInt(request.getParameter("code")));
 //		comBoardVal.setCode((int) Integer.parseInt("2") );
 
 		qnaService.qnaDetail(comBoardVal);
 
-		map.put("qnaList", qnaService.qnaDetail(comBoardVal));
+		map.put("qnaDetail", qnaService.qnaDetail(comBoardVal));
 		return map;
 	}
 
